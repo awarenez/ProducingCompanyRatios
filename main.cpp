0e04b9f782e5f610f2c9eef3eb8a2e5159b52432
Dennis Chang <ccy.dennis@gmail.com>
2014-03-13 15:25:01 -0400

    finish calculating the ratios

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <string>
 #include <cstdio>
-#include <memory>
+
+#include <vector>
+#include <map>
 
 #include "DataPoint.h"
 
@@ -25,15 +26,12 @@ void SetDate(DataPoint* pd, const string &s, int index) {
     if (!pd) return;
     switch (index) {
     case DAY:
-        //cout << "Day: " << atoi(s.c_str()) << endl;
         pd->setDay(atoi(s.c_str()));
         break;
     case MONTH:
-        //cout << "Month: " << atoi(s.c_str()) << endl;
         pd->setMonth(atoi(s.c_str()));
         break;
     case YEAR:
-        //cout << "Year: " << atoi(s.c_str()) << endl;
         pd->setYear(atoi(s.c_str()));
         break;
     default:
@@ -78,30 +76,174 @@ void SetField(DataPoint* pd, const string &field, int index) {
     }
 }
 
-vector<float> getAnnualPE(const vector<DataPoint*> &v) {
+// P/E = current stock price over trailing EPS
+vector<float> getAnnualPE(map<int, vector<DataPoint*>, greater<int>> mymap) {
     vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            int num_of_quarters = v.size();
+            if (num_of_quarters != 4) continue; // make sure there are four quarters
+            float fTrailingPE = 0.0F;
+            for (int i = 0; i < static_cast<int>(v.size()); i++) {
+                float fQuarterlyPE = v[i]->getLastPx() / v[i]->getEPS(); // P/E = stock price over EPS
+                fTrailingPE += fQuarterlyPE;
+            }
+            res.emplace_back(fTrailingPE);
+        }
+    }
     return res;
 }
 
-vector<float> getTrailingPE(const vector<DataPoint*> &v) {
+vector<float> getTrailingPE(map<int, vector<DataPoint*>, greater<int>> mymap) {
     vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            int num_of_quaters = static_cast<int>(v.size()); 
+            if (num_of_quaters != 4) continue;
+            float fCurrentPrice = v[0]->getLastPx();
+            float fTrailingEPS = 0.0F;
+            for (int i = 0; i < num_of_quaters; i++) {
+                fTrailingEPS += v[i]->getEPS(); 
+            }
+            float fAnnualPE = fCurrentPrice / fTrailingEPS;
+            res.emplace_back(fAnnualPE);
+        }
+    }
+    return res;
+}
+
+vector<float> getQuarterlyPE(map<int, vector<DataPoint*>, greater<int>> mymap) {
+    vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            float fTrailingPE = 0.0F;
+            for (int i = 0; i < static_cast<int>(v.size()); i++) {
+                if (v[i]->getMonth() % 3 != 0 || v[i]->getMonth() < 1 || v[i]->getMonth() > 12) continue;
+                float fQuarterlyPE = v[i]->getLastPx() / v[i]->getEPS(); // P/E = stock price over EPS
+                res.emplace_back(fQuarterlyPE);
+            }
+        }
+    }
+    return res;
+}
+
+vector<float> getYoyEPSGrowth(map<int, vector<DataPoint*>, greater<int>> mymap) {
+    vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            int num_of_quarters = static_cast<int>(v.size());
+            if (num_of_quarters != 4) continue; // make sure there are four quarters
+            for (int i = 0; i < num_of_quarters; i++) {
+                if (v[i]->getMonth() == 12) {
+                    res.emplace_back(v[i]->getEPS());
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < static_cast<int>(res.size() - 1); i++) {
+        res[i] = (res[i] - res[i + 1]) / res[i + 1] * 100.0F; // in percentage
+    }
+    res.pop_back();
+
+    return res;
+}
+
+vector<float> getYoyRevenueGrowth(map<int, vector<DataPoint*>, greater<int>> mymap) {
+    vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            int num_of_quarters = v.size();
+            if (num_of_quarters != 4) continue; // make sure there are four quarters
+            float yearlyRevenue = 0;
+            for (int i = 0; i < num_of_quarters; i++) {
+                yearlyRevenue += v[i]->getRevenue();
+            }
+            res.emplace_back(yearlyRevenue);
+        }
+    }
+
+    for (int i = 0; i < static_cast<int>(res.size() - 1); i++) {
+        res[i] = (res[i] - res[i + 1]) / res[i + 1] * 100.0F; // growth in percentage
+    }
+    res.pop_back();
+
+    return res;
+}
+
+vector<float> getQoqEPSGrowth(map<int, vector<DataPoint*>, greater<int>> mymap) {
+    vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            for (int i = 0; i < static_cast<int>(v.size()); i++) {
+                if (v[i]->getMonth() % 3 != 0 || v[i]->getMonth() < 1 || v[i]->getMonth() > 12) continue;
+                res.emplace_back(v[i]->getEPS());
+            }
+        }
+    }
+
+    for (int i = 0; i < static_cast<int>(res.size() - 1); i++) {
+        res[i] = (res[i] - res[i + 1]) / res[i + 1] * 100.0F; // growth in percentage
+    }
+    res.pop_back();
+
+    return res;
+}
+
+vector<float> getQoqRevenueGrowth(map<int, vector<DataPoint*>, greater<int>> mymap) {
+    vector<float> res;
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = mymap.begin(); mapit != mymap.end(); mapit++) {
+        // get vector according to year
+        if (mapit->first != -1) {
+            vector<DataPoint*> v = mapit->second;
+            for (int i = 0; i < static_cast<int>(v.size()); i++) {
+                if (v[i]->getMonth() % 3 != 0 || v[i]->getMonth() < 1 || v[i]->getMonth() > 12) continue;
+                res.emplace_back(v[i]->getRevenue());
+            }
+        }
+    }
+
+    for (int i = 0; i < static_cast<int>(res.size() - 1); i++) {
+        res[i] = (res[i] - res[i + 1]) / res[i + 1] * 100.0F; // growth in percentage
+    }
+    res.pop_back();
+
     return res;
 }
 
 int main(int argc, const char * argv[])
 {
     // Read Data Points
-    vector<DataPoint*> v;
+    map<int, vector<DataPoint*>, greater<int>> datamap; // use year as key
     
     // FILE I/O
-    //string input_fname("D:/Projects/ProducingCompanyRatios/input[1].txt");
-    string input_fname(argv[1]);
+    string input_fname("D:/Projects/ProducingCompanyRatios/input[1].txt");
+    //string input_fname(argv[1]);
     ifstream ifs;
     ifs.open(input_fname.c_str());
     string line;
-    if (ifs.is_open()) { // Read header
+    if (ifs.is_open()) {
+        // Read header
         getline(ifs, line);
-        //cout << line << endl;
 
         // Read data from each line
         while (ifs.good()) {
@@ -110,39 +252,94 @@ int main(int argc, const char * argv[])
             DataPoint* pd = new DataPoint;
             while ((end = static_cast<int>(line.find("\t",start))) != string::npos) {
                 string field = line.substr(start, end - start);
-                //cout << field << endl;
                 SetField(pd, field, nFieldIdx);
-                
                 start = end + 1;
                 ++nFieldIdx;
             }
             SetField(pd, line.substr(start), nFieldIdx);
-            v.push_back(pd);
-            //cout << line.substr(start) << endl; // BVPS
-            //cout << line.c_str() << endl;
+            datamap[pd->getYear()].emplace_back(pd);
         }
     }
 
-    int nDataPoints = static_cast<int>(v.size());
-    cout << "Data Points: " << nDataPoints << endl;
-    // Calculations
-    for (int i = 0; i < nDataPoints; i++) {
-        cout << v[i]->getMonth() << "/" << v[i]->getDay() << "/" << v[i]->getYear() << " " <<
+    map<int, vector<DataPoint*>, greater<int>>::iterator mapit;
+    for (mapit = datamap.begin(); mapit != datamap.end(); mapit++) {
+        vector<DataPoint*> v = mapit->second;
+        for (int i = 0; i < static_cast<int>(v.size()); i++) {
+             cout << v[i]->getMonth() << "/" << v[i]->getDay() << "/" << v[i]->getYear() << " " <<
              v[i]->getRevenue() << " " << v[i]->getEPS() << " " << v[i]->getLastPx() << " " <<
              v[i]->getBVPS() << endl;
+        }
     }
+    cout << endl << endl;
+
+    // TODO: Sort the data in each year
+
     
     // Get Annual P/E
-    vector<float> AnnualPE = getAnnualPE(v);
+    vector<float> annualPE = getAnnualPE(datamap);
+    cout << "Annual P/E: ";
+    for (int i = 0; i < static_cast<int>(annualPE.size()); i++) {
+        cout << annualPE[i] << ", ";
+    }
+    cout << endl;
     
     // Get Tailing P/E
-    vector<float> TrailingPE = getTrailingPE(v);
-
-    // Delete Data Points
-    for (int i = 0; i < nDataPoints; i++) {
-        delete v[i];
+    vector<float> trailingPE = getTrailingPE(datamap);
+    cout << "Trailing P/E: ";
+    for (int i = 0; i < static_cast<int>(trailingPE.size()); i++) {
+        cout << trailingPE[i] << ", ";
     }
+    cout << endl;
+
+    //Get Quarterly P/E
+    vector<float> quarterlyPE = getQuarterlyPE(datamap);
+    cout << "Quarterly P/E: ";
+    for (int i = 0; i < static_cast<int>(quarterlyPE.size()); i++) {
+        cout << quarterlyPE[i] << ", ";
+    }
+    cout << endl;
+
+
+    // Get year over year EPS growth
+    vector<float> yoyEPSGrowth = getYoyEPSGrowth(datamap);
+    cout << "Yoy EPS Growth: ";
+    for (int i = 0; i < static_cast<int>(yoyEPSGrowth.size()); i++) {
+        cout << yoyEPSGrowth[i] << ", ";
+    }
+    cout << endl;
+
+    // Get year over year Revenue growth
+    vector<float> yoyRevenueGrowth = getYoyRevenueGrowth(datamap);
+    cout << "Yoy Revenue Growth: ";
+    for (int i = 0; i < static_cast<int>(yoyRevenueGrowth.size()); i++) {
+        cout << yoyRevenueGrowth[i] << ", ";
+    }
+    cout << endl;
+
+    // Get quarter over quarter EPS growth
+    vector<float> qoqEPSGrowth = getQoqEPSGrowth(datamap);
+    cout << "Qoq EPS Growth: ";
+    for (int i = 0; i < static_cast<int>(qoqEPSGrowth.size()); i++) {
+        cout << qoqEPSGrowth[i] << ", ";
+    }
+    cout << endl;
+
+    // Get quarter over quater revenue growth
+    vector<float> qoqRevenueGrowth = getQoqRevenueGrowth(datamap);
+    cout << "Qoq Revenue Growth: ";
+    for (int i = 0; i < static_cast<int>(qoqRevenueGrowth.size()); i++) {
+        cout << qoqRevenueGrowth[i] << ", ";
+    }
+    cout << endl;
     
+    // Delete Data Points in map
+    for (mapit = datamap.begin(); mapit != datamap.end(); mapit++) {
+        vector<DataPoint*> v = mapit->second;
+        for (int i = 0; i < static_cast<int>(v.size()); i++) {
+            delete v[i];
+        }
+    }
+
     return 0;
 }
 
